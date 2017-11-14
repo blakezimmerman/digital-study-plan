44c08393bd9246b25213d54eeba1d915c23c5bfc
Blake Zimmerman <blake.zimmerman@icloud.com>
2017-11-14 00:06:55 -0500

    Add initial study plans

---
+++ src/client/shared/studyPlans.js
@@ -0,0 +1,48 @@
+export const emptyPlan = {
+  programs: {
+    major: undefined,
+    minor: undefined
+  },
+  semesters: []
+};
+
+export const majors = [
+  {
+    program: 'Software Engineering',
+    type: 'Major',
+    semesters: [
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ]
+  },
+  {
+    program: 'Mechanical Engineering',
+    type: 'Major',
+    semesters: [
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ]
+  }
+];
+
+export const minors = [
+  {
+    program: 'Computer Science',
+    type: 'Minor',
+    courses:
+      [
+      ]
+  }
+]
